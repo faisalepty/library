10f9abc09e1c6deac7364dd1e66d9acfdc382333
Faisalepty <jojomadready@gmail.com>
2024-02-01 14:59:58 +0300

    update

---
+++ staticfiles/js/addMember.js
@@ -0,0 +1,86 @@
+const addMemberFormBtn = document.getElementById('addmember');
+const photoInput = document.getElementById('imageFile');
+const imgField = document.getElementById('imgField');
+const csrfTokenInput = document.getElementById('modal1csrf_token').firstChild;
+const memberModal = document.getElementById('myModal1');
+
+const formData = new FormData();
+const formInputs = ['first_name', 'last_name', 'gender', 'age', 'address', 'mobile_no', 'email', 'national_id'];
+
+const resetMemberInputs = () => {
+    formInputs.forEach(inputName => {
+        const input = document.getElementsByName(inputName)[0];
+        input.value = '';
+    });
+    imgField.querySelector('img').classList.add('d-none');
+    imgField.querySelector('label').classList.remove('d-none');
+    imgField.querySelector('input').classList.remove('d-none');
+};
+
+const updatememberbtn = document.getElementById('updateMember');
+const addmemberbtn = document.getElementById('addmember');
+updatememberbtn.classList.add('d-none');
+addmemberbtn.classList.remove('d-none');
+memberModal.getElementsByClassName('modal-title')[0].firstChild.innerText = 'ADD MEMBER'
+
+formInputs.forEach(inputName => {
+    const inputs = document.getElementsByName(inputName);
+    inputs.forEach(input => {
+        input.addEventListener('change', (e) => {
+            console.log(input, input.value);
+            formData.append(inputName, input.value);
+        });
+    });
+});
+
+formData.append('debt', 0);
+formData.append('csrfmiddlewaretoken', csrfTokenInput.value);
+
+photoInput.addEventListener('change', () => {
+    formData.append("profile_img", photoInput.files[0]);
+    if (photoInput.files.length > 0) {
+        imgField.childNodes.forEach(node => {
+            if (node.nodeType === 1) { 
+                node.classList.add('d-none');
+            }
+        });
+        const lastChild = imgField.lastChild;
+        if (lastChild.previousSibling) {
+            lastChild.previousSibling.classList.remove('d-none');
+        } 
+    }
+});
+
+addMemberFormBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    $.ajax({
+        type: 'POST',
+        url: '/newmember/',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        processData: false,
+        contentType: false,
+        enctype: 'multipart/form-data',
+        data: formData,
+        success: (res) => {
+            if (res.success){
+            resetMemberInputs();
+            memberModal.classList.remove('show');
+            memberModal.style.display = 'none';
+            successAlert(res.success);
+        }else if (res.error){
+            memberModal.classList.remove('show');
+            memberModal.style.display = 'none';
+            errorAlert(res.error)
+        }
+    },
+        error: (res) => {
+            memberModal.classList.remove('show');
+            memberModal.style.display = 'none';
+            errorAlert(res.error)
+        }
+    });
+});
---
+++ staticfiles/js/updateBook.js
@@ -0,0 +1,119 @@
+const updateBookModal = document.getElementById('myModal')
+const updateBookBtn = document.getElementsByClassName('updateBookBtn')
+const buttonsArr = [...updateBookBtn]
+let status
+buttonsArr.forEach(btn => {
+  btn.addEventListener('click', (e) =>{
+    id = e.target.id
+  $.ajax({
+    type:'GET',
+    url: `/editbookinfo/${id}`,
+    success: (res) => {
+      console.log(res)
+      const titleInput = document.getElementsByName('title')[0];
+    const authorInput = document.getElementsByName('author')[0];
+    const descriptionInput = document.getElementsByName('description')[0];
+    const languageInput = document.getElementsByName('language')[0];
+    const categoryInput = document.getElementsByName('category')[0];
+    const editionInput = document.getElementsByName('edition')[0];
+    const pagesInput = document.getElementsByName('pages')[0];
+    const publisherInput = document.getElementsByName('publisher')[0];
+    const publishDateInput = document.getElementsByName('publish_date')[0];
+    const imgUrlInput = document.getElementsByName('img_url')[0];
+    const quantityInput = document.getElementsByName('quantity')[0];
+    const feeInput = document.getElementsByName('fee')[0];
+    status = res.data.status
+
+    titleInput.value = `${res.data.title}`;
+            authorInput.value = `${res.data.author}`;
+            descriptionInput.value = `${res.data.description}`;
+            languageInput.value = `${res.data.language}`;
+            categoryInput.value = `${res.data.category}`;
+            editionInput.value = `${res.data.edition}`;
+            pagesInput.value = `${res.data.pages}`;
+            publisherInput.value = `${res.data.publisher}`;
+            publishDateInput.value = `${res.data.publish_date}`;
+            imgUrlInput.value = `${res.data.img_url}`;
+            quantityInput.value = `${res.data.quantity}`;
+            feeInput.value = `${res.data.fee}`;
+    addBookFormBtn.classList.add('d-none')
+    const updateBtn = document.getElementById('updatebtn') 
+    updateBtn.style.display = 'block'
+    const title = updateBookModal.getElementsByClassName('modal-title')[0].firstChild
+    title.innerText = 'UPDATE BOOK'
+    }
+
+  })
+  })
+})
+
+
+const updateBtn = document.getElementById('updatebtn')
+
+updateBtn.addEventListener('click', (e) => {
+  const titleInput = document.getElementsByName('title')[0];
+    const authorInput = document.getElementsByName('author')[0];
+    const descriptionInput = document.getElementsByName('description')[0];
+    const languageInput = document.getElementsByName('language')[0];
+    const categoryInput = document.getElementsByName('category')[0];
+    const editionInput = document.getElementsByName('edition')[0];
+    const pagesInput = document.getElementsByName('pages')[0];
+    const publisherInput = document.getElementsByName('publisher')[0];
+    const publishDateInput = document.getElementsByName('publish_date')[0];
+    const imgUrlInput = document.getElementsByName('img_url')[0];
+    const quantityInput = document.getElementsByName('quantity')[0];
+    const feeInput = document.getElementsByName('fee')[0]; 
+    const csrfTokenInput = document.getElementById('csrf_token').firstChild;
+  
+
+    const datar = {
+        'title': titleInput.value,
+        'author': authorInput.value,
+        'description': descriptionInput.value,
+        'language': languageInput.value,
+        'category': categoryInput.value,
+        'edition': editionInput.value,
+        'pages': pagesInput.value,
+        'publisher': publisherInput.value,
+        'publish_date': publishDateInput.value,
+        'img_url': imgUrlInput.value,
+        'quantity': quantityInput.value,
+        'fee': feeInput.value,
+        'status': status,
+        'csrfmiddlewaretoken': csrfTokenInput.value
+    };
+        console.log(datar)
+      $.ajax({
+        type: 'POST',
+        url: `/editbookinfo/${id}`,
+        headers: {'X-Requested-With': 'XMLHttpRequest'
+      },
+        data: datar,
+        success: (res) => {
+          if(res.success){
+          const inputs = Object.values(datar)
+          titleInput.value = '';
+            authorInput.value = '';
+            descriptionInput.value = '';
+            languageInput.value = '';
+            categoryInput.value = '';
+            editionInput.value = '';
+            pagesInput.value = '';
+            publisherInput.value = '';
+            publishDateInput.value = '';
+            imgUrlInput.value = '';
+            quantityInput.value = '';
+            feeInput.value = '';
+            updateBookModal.classList.remove('show')
+            updateBookModal.style.display = 'none'
+            successAlert()}else if(res.error){
+              errorAlert(res.error)
+            }
+        },
+        error: (res) => {
+          console.log(res)
+        }
+
+      })
+})
+
