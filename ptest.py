c2da8cfc9c084fb1e91d34711c83060d09cab458
Faisalepty <jojomadready@gmail.com>
2024-02-05 11:24:30 +0300

    update

--- ptest.py
+++ ptest.py
@@ -5,9 +5,9 @@ from main.models import Book
 with open('../data/books_1.Best_Books_Ever.csv', 'r', newline='', encoding='utf-8') as file:
 
     data = csv.DictReader(file)
-    i = 150
+    i = 250
     for line in data:
-        if i < 180:
+        if i < 320:
             title = line['title']
             print(title)
             author = line['author']
@@ -28,3 +28,26 @@ with open('../data/books_1.Best_Books_Ever.csv', 'r', newline='', encoding='utf-
             print(f" 'title': {title}, \n 'author': {author}, \n ")
             Book.objects.create(title=title, author=author, description=description, language=language, category=genres, edition=edition, pages=pages, publisher=publisher, publish_date=publish_date, img_url=img_url, quantity=quantity, fee=fee, status='On shelf')
             i = i + 1
+
+
+
+
+import csv
+from main.models import Book
+
+with open('../data/books_1.Best_Books_Ever.csv', 'r', newline='', encoding='utf-8') as file:
+    data = csv.DictReader(file)
+    i = 250
+    for line in data:
+        if i < 320:
+            title = line['title']
+            author = line['author']
+            description = line['description']
+            language = line['language']
+            genres = f'{line["genres"].split(",")[2][2:-1]}, {line["genres"].split(",")[3][2:-1]}, {line["genres"].split(",")[4][2:-1]}, {line["genres"].split(",")[5][2:-1]}'
+            edition = line['edition']
+            pages = line['pages']
+
+            Book.objects.create(title=title, author=author, description=description, language=language, genres=genres, edition=edition, pages=pages)
+
+            i += 1
